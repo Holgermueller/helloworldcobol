9f0ff1693a4ca46caf86131f52fcd3d1b6cc59b0
holgermueller <9jholger@gmail.com>
2024-07-16 16:36:49 -0500

    some code

---
+++ helloworld.cbl
@@ -0,0 +1,7 @@
+      * run cobc -x helloworld-cbl to compile.
+      * run ./helloworld to run the compile program
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO.
+       PROCEDURE DIVISION.
+           DISPLAY "Hello, world".
+       END PROGRAM HELLO.
